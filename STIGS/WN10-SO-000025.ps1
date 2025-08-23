ff8121afd468dafa0f8976ab2cb0ec232297a620
Joshua Schlegel <168645119+JoshuaSchlegel@users.noreply.github.com>
2025-08-23 09:12:39 -0500

    Create WN10-SO-000025.ps1

---
+++ STIGS/WN10-SO-000025.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the built-in guest account is renamed.
+
+.NOTES
+    Author          : Joshua Schlegel
+    LinkedIn        : linkedin.com/in/joshuaschlegel/
+    GitHub          : github.com/joshuaschlegel
+    Date Created    : 2025-08-23
+    Last Modified   : 2025-08-23
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000025
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-SO-000025.ps1 
+#>
+
+Rename-LocalUser -Name "Guest" -NewName "GstLocal"
